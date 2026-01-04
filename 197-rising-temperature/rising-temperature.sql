15faeb3be407db4b3f286f8ffa4f8abc9a06e9e1
Billakurthi Prudhvinadh Satya Sasankadhar Reddy <billakurthiprudhvireddy@gmail.com>
2026-01-04 21:37:42 +0530

    Time: 507 ms (21.52%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-select today.id from weather as yesterday cross join weather today
+select today.id from weather as yesterday join weather today
 where datediff(today.recordDate,yesterday.recordDate) = 1
     and today.temperature>yesterday.temperature
