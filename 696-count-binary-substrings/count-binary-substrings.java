c4cadfefc9d59faa11290dfd9bb981aa823c2d1f
Billakurthi Prudhvinadh Satya Sasankadhar Reddy <billakurthiprudhvireddy@gmail.com>
2026-02-20 00:37:04 +0530

    Time: 11 ms (58.70%) | Memory: 46.3 MB (72.72%) - LeetSync

---
+++ 696-count-binary-substrings/count-binary-substrings.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int p=0,c=1,r=0;
+        for(int i=1;i<s.length();i++){
+            if(s.charAt(i)==s.charAt(i-1)){
+                c++;
+            }
+            else{
+                r+=Math.min(p,c);
+                p=c;
+                c=1;
+            }
+        }
+        r+=Math.min(p,c);
+        return r;
+    }
+}
