0094585009f71e75f8dc8068edc4a2d003368f74
Billakurthi Prudhvinadh Satya Sasankadhar Reddy <billakurthiprudhvireddy@gmail.com>
2026-09-21 20:47:41 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (18.68%) - LeetSync

---
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countNumbersWithUniqueDigits(int n) {
+        if (n == 0) return 1;   
+        int ans = 10;           
+        int uniqueDigits = 9;   
+        int availableDigits = 9; 
+
+        for (int i = 2; i <= n; i++) {
+            uniqueDigits *= availableDigits; 
+            ans += uniqueDigits;             
+            availableDigits--;              
+        }
+        return ans;
+    }
+}
